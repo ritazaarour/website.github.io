8f8e4d7a68167aee3186e62a8f920c1af6a5ea10
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-15 17:12:26 -0700

    Update global.js

--- global.js
+++ global.js
@@ -33,4 +33,9 @@ for (let p of pages) {
   let title = p.title;
 
   url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
 }
