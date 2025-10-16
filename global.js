e7cd39aca7600870bc1671b4119eee74201e586e
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-15 17:02:55 -0700

    testing javascript nav

--- global.js
+++ global.js
@@ -3,3 +3,26 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/'
+    : '/website/';
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/ritazaarour', title: 'GitHub' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+}
