2d3dd3db9a8f59a33635825e124b759fa98f9bb4
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-21 16:20:35 -0700

    creating renders project function

--- global.js
+++ global.js
@@ -88,3 +88,16 @@ export async function fetchJSON(url) {
     console.error('Error fetching or parsing JSON data:', error);
   }
 }
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+  const article = document.createElement('article');
+}
+
+article.innerHTML = `
+    <h3>${project.title}</h3>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+`;
+
+containerElement.appendChild(article);
