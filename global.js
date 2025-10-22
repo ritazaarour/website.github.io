4af3d35878f225cf499e9e7416199f68479d6629
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-21 20:59:13 -0700

    Update global.js

--- global.js
+++ global.js
@@ -91,12 +91,14 @@ export async function fetchJSON(url) {
 
 export function renderProjects(project, containerElement, headingLevel = 'h2') {
   containerElement.innerHTML = '';
+
   const article = document.createElement('article');
 
   article.innerHTML = `
     <h3>${project.title}</h3>
     <img src="${project.image}" alt="${project.title}">
     <p>${project.description}</p>
-`;
-containerElement.appendChild(article);
+  `;
+  
+  containerElement.appendChild(article);
 }
