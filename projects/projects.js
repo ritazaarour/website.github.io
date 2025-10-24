737915e421c37885ceffb0f8195cb196f54e3d72
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-23 20:15:20 -0700

    displaying projects on home page

--- projects/projects.js
+++ projects/projects.js
@@ -3,7 +3,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 
 const titleElement = document.querySelector('.projects-title');
-titleElement.textContent = `Projects (${projects.length})`;
+titleElement.textContent = `${projects.length} Projects`;
 
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
