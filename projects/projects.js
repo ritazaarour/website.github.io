29b596ff5f34a2b82e31b102c3c22f582cb73e81
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-23 20:05:50 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,15 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
-console.log(projects);
-
-const projectsContainer = document.querySelector('.projects');
-
-renderProjects(projects, projectsContainer, 'h2');
-
-import { fetchJSON, renderProjects } from '../global.js';
-
-const projects = await fetchJSON('../lib/projects.json');
 
 const titleElement = document.querySelector('.projects-title');
 titleElement.textContent = `Projects (${projects.length})`;
