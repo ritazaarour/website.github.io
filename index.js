08d6b4694fc07283686f9582ee37120324ef4f90
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-23 21:58:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
@@ -6,7 +6,8 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
 
-import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+
+
 const githubData = await fetchGitHubData('ritazaarour');
 const profileStats = document.querySelector('#profile-stats');
 
