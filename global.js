d79ef237a2e63a1039234fa7741250d38c7c19f0
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-23 20:02:44 -0700

    fixed json and added project counter

--- global.js
+++ global.js
@@ -89,16 +89,16 @@ export async function fetchJSON(url) {
   }
 }
 
-export function renderProjects(project, containerElement, headingLevel = 'h2') {
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
   containerElement.innerHTML = '';
 
-  const article = document.createElement('article');
-
-  article.innerHTML = `
-    <${headingLevel}>${project.title}</${headingLevel}>
-    <img src="${project.image}" alt="${project.title}">
-    <p>${project.description}</p>
-  `;
-
-  containerElement.appendChild(article);
+  projects.forEach(project => {
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  });
 }
--- projects/projects.js
+++ projects/projects.js
@@ -6,3 +6,13 @@ console.log(projects);
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+import { fetchJSON, renderProjects } from '../global.js';
+
+const projects = await fetchJSON('../lib/projects.json');
+
+const titleElement = document.querySelector('.projects-title');
+titleElement.textContent = `Projects (${projects.length})`;
+
+const projectsContainer = document.querySelector('.projects');
+renderProjects(projects, projectsContainer, 'h2');
