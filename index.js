45234a56a9b631103407001e32c99bffebaa8875
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-23 21:50:33 -0700

    bug fix

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
 
-const githubData = await fetchGitHubData('giorgianicolaou');
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+const githubData = await fetchGitHubData('ritazaarour');
 const profileStats = document.querySelector('#profile-stats');
 
 if (profileStats) {
   profileStats.innerHTML = `
         <dl>
           <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
           <dt>Followers:</dt><dd>${githubData.followers}</dd>
           <dt>Following:</dt><dd>${githubData.following}</dd>
         </dl>
