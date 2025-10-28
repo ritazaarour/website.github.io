0e475d59ed74674a8abccbb178066f25f3a64477
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-27 20:52:15 -0700

    fixing year in projects

--- global.js
+++ global.js
@@ -97,8 +97,10 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
-      <p><em>${project.year}</em></p> 
+      <div class="project-details">
+        <p>${project.description}</p>
+        <p><em>${project.year}</em></p>
+      </div>
     `;
     containerElement.appendChild(article);
   });
