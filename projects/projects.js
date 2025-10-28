19e7d866ab6449490c19071fcbc2e91ceae76e82
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-27 21:55:09 -0700

    adding chart with d3

--- projects/projects.js
+++ projects/projects.js
@@ -7,3 +7,11 @@ titleElement.textContent = `${projects.length} Projects`;
 
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: (2 * Math.PI)  
+});
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
