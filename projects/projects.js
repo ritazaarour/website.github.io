eb62cbd21665c53978a0abcedb633a49ac8a1ebd
Rita Zaarour <ritazaarour9@gmail.com>
2025-10-28 17:51:59 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -19,29 +19,13 @@ d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
 //drawing static pie chart 
 let data = [1, 2];
-
-let total = 0;
-
-for (let d of data) {
-  total += d;
-}
-
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
-
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
-
 arcs.forEach((arc, idx) => {
     d3.select('svg')
       .append('path')
       .attr('d', arc)
       .attr('fill', colors[idx % colors.length]);
 });
-
 let colors = ['gold', 'purple'];
