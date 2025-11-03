814df896ab571bd25546f5543c5317f8224c9685
Rita Zaarour <ritazaarour9@gmail.com>
2025-11-02 16:55:26 -0800

    updating and reorganizing projects

--- projects/projects.js
+++ projects/projects.js
@@ -9,7 +9,7 @@ const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arcGenerator = d3.arc().innerRadius(20).outerRadius(50);
 let arc = arcGenerator({
   startAngle: 0,
   endAngle: (2 * Math.PI)  
@@ -39,10 +39,11 @@ arcs.forEach((arc, idx) => {
 //legend
 let legend = d3.select('.legend');
 data.forEach((d, idx) => {
-    legend.append('li')
-          .attr('style', `--color:${colors(idx)}`)
-          .attr('class', 'legend-item')
-          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+    legend
+      .append('li')
+      .attr('style', `--color:${colors(idx)}`)
+      .attr('class', 'legend-item')
+      .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
 
 //search bar
@@ -71,7 +72,6 @@ function renderPieChart(projectsGiven) {
   let newSliceGenerator = d3.pie().value((d) => d.value);
   let newArcData = newSliceGenerator(newData);
   let newArcs = newArcData.map((d) => arcGenerator(d));
-  // TODO: clear up paths and legends
   // Remove existing pie chart paths
   d3.select('svg').selectAll('path').remove();
   // Remove existing legend items
