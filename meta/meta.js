ae44f55c6a78d2a2a19ee377662ff579bfe4d36c
Rita Zaarour <ritazaarour9@gmail.com>
2025-11-03 20:33:12 -0800

    Update meta.js

--- meta/meta.js
+++ meta/meta.js
@@ -1,5 +1,6 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
+
 // read csv 
 // row conversion function
 async function loadData() {
@@ -15,6 +16,36 @@ const data = await d3.csv('loc.csv', (row) => ({
   return data;
 }
 
-let data = await loadData();
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/website.github.io/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
 
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        writable: false,
+        enumerable: true,
+        configurable: true
+      });
+
+      return ret;
+    });
+}
+
+let data = await loadData();
+let commits = processCommits(data);
+console.log(commits);
 
